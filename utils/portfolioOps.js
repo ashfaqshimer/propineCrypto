9bdb9f649831f2e876a58040f47b506f8456b137
Ashfaq Shimer <shimer.ahamed@gmail.com>
2022-07-10 00:23:58 +0530

    Optimize get date and token

--- utils/portfolioOps.js
+++ utils/portfolioOps.js
@@ -2,10 +2,7 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const { TRANSACTIONS_PATH } = require('../constants');
 const { getConversionRate } = require('../services/cryptoCompare');
-const {
-	convertTimestampToDate,
-	convertDateToUnixTimestamp,
-} = require('./dateConversion');
+const { convertDateToUnixTimestamp } = require('./dateConversion');
 
 exports.getLatestPortfolio = async () => {
 	console.log('Getting latest portfolio values');
@@ -44,46 +41,6 @@ exports.getPortfolioByDate = (thresholdDate) => {
 			console.log(portfolio);
 		});
 };
-// exports.getPortfolioByDate = (date) => {
-// const splitDate = date.split('/').map((value) => parseInt(value));
-
-// 	console.log('Getting portfolio for date ', date);
-// 	const results = [];
-
-// 	console.log('Reading CSV..');
-// 	const readStream = fs
-// 		.createReadStream(TRANSACTIONS_PATH)
-// 		.pipe(csv())
-// 		.on('data', (data) => {
-// 			const transactionDate = convertTimestampToDate(data.timestamp);
-
-// 			if (transactionDate.year < splitDate[2]) {
-// 				readStream.destroy();
-// 			}
-
-// 			if (transactionDate.year === splitDate[2]) {
-// 				if (transactionDate.month < splitDate[0]) {
-// 					readStream.destroy();
-// 				}
-// 				if (transactionDate.month === splitDate[0]) {
-// 					if (transactionDate.date < splitDate[1]) {
-// 						readStream.destroy();
-// 					}
-// 					if (transactionDate.date === splitDate[1]) {
-// 						results.push(data);
-// 					}
-// 				}
-// 			}
-// 		})
-// 		.on('close', async () => {
-// 			if (results.length) {
-// 				const portfolio = await calculatePortfolio(results);
-// 				console.log(portfolio);
-// 			} else {
-// 				console.log('No results found');
-// 			}
-// 		});
-// };
 
 exports.getPortfolioByToken = (token) => {
 	console.log(`Getting latest portfolio for ${token}`);
@@ -105,75 +62,26 @@ exports.getPortfolioByToken = (token) => {
 		});
 };
 
-exports.getPortfolioByTokenAndDate = (token, date) => {
-	console.log(`Getting portfolio on date ${date} for token ${token}`);
-	const splitDate = date.split('/').map((value) => parseInt(value));
-	const results = [];
+exports.getPortfolioByTokenAndDate = (token, thresholdDate) => {
+	console.log(`Getting portfolio as at ${thresholdDate} for token ${token}`);
+	const timestampThreshold = convertDateToUnixTimestamp(thresholdDate);
+	let portfolio = {};
 
-	const readStream = fs
-		.createReadStream(TRANSACTIONS_PATH)
+	console.log('Reading CSV..');
+	fs.createReadStream('./_data/test.csv')
 		.pipe(csv())
 		.on('data', (data) => {
-			const transactionDate = convertTimestampToDate(data.timestamp);
-
-			if (transactionDate.year < splitDate[2]) {
-				readStream.destroy();
-			}
-
-			if (transactionDate.year === splitDate[2]) {
-				if (transactionDate.month < splitDate[0]) {
-					readStream.destroy();
-				}
-				if (transactionDate.month === splitDate[0]) {
-					if (transactionDate.date < splitDate[1]) {
-						readStream.destroy();
-					}
-					if (transactionDate.date === splitDate[1]) {
-						if (data.token === token) {
-							results.push(data);
-						}
-					}
-				}
+			if (data.timestamp < timestampThreshold && data.token === token) {
+				portfolio = calculatePortfolio(portfolio, data);
 			}
 		})
 		.on('close', async () => {
-			if (results.length) {
-				const portfolio = await calculatePortfolio(results);
-				console.log(portfolio);
-			} else {
-				console.log('No results found');
-			}
+			console.log('Finished reading data. Portfolio amounts:');
+			console.log(portfolio);
+			portfolio = await calculateCurrencyPortfolio(portfolio, 'USD');
+			console.log(portfolio);
 		});
 };
-
-// const calculatePortfolio = async (transactions) => {
-// 	const transactionsObject = transactions.reduce((accumulator, transaction) => {
-// 		const { token, transaction_type } = transaction;
-// 		const amount = parseFloat(transaction.amount);
-// 		if (!accumulator[token]) {
-// 			accumulator[token] = 0;
-// 		}
-
-// 		if (transaction_type === 'DEPOSIT') {
-// 			accumulator[token] = accumulator[token] + amount;
-// 		}
-// 		if (transaction_type === 'WITHDRAWAL') {
-// 			accumulator[token] = accumulator[token] - amount;
-// 		}
-
-// 		return accumulator;
-// 	}, {});
-
-// 	const tokens = Object.keys(transactionsObject).join(',');
-
-// 	const exchangeRate = await getConversionRate(tokens, 'USD');
-
-// 	for (const token in transactionsObject) {
-// 		transactionsObject[token] =
-// 			transactionsObject[token] * exchangeRate[token].USD;
-// 	}
-// 	return transactionsObject;
-// };
 
 const calculatePortfolio = (currentPortfolio, transaction) => {
 	const updatedPortfolio = { ...currentPortfolio };
